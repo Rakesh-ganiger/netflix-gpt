8c3adb989349d9f156bc64ca0a3c8b8c781bdbfe
Rakesh-ganiger <ganigerrakesh1709@gmail.com>
2025-01-10 21:13:58 +0530

    Sign in and signup form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-
-import './App.css';
+import Body from "./components/Body"
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='font-bold text-center'>Hello</h1>
-    </div>
+    <Body/>
   );
 }
 
